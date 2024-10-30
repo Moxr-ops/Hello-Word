b75e7539af67aeac21b86eb631c4e2ad2ca0e971
Moxr <2261750119@qq.com>
2024-10-30 08:13:25 +0800

    修改作业

--- Homework/0510.cpp
+++ Homework/0510.cpp
@@ -1,36 +1,25 @@
 #include <stdio.h>
-#include <string>
-using namespace std;
 
-// 判断一个数是否为完数并打印其因子
-void printPerfectNumber(int num) {
-    int sum = 0;
-    string factors = "";
-    
-    // 寻找因子
-    for(int i = 1; i < num; i++) {
-        if(num % i == 0) {
-            sum += i;
-            // 添加因子到字符串
-            if(factors.empty()) {
-                factors = to_string(i);
-            } else {
-                factors += "," + to_string(i);
-            }
-        }
+int Fibonacci(int n) {
+    if (n <= 1) {
+        return 1;
     }
-    
-    // 如果是完数，则打印结果
-    if(sum == num) {
-        printf("%d its factors are %s\n", num, factors.c_str());
+    int a = 1, b = 1, sum;
+    for (int i = 2; i <= n; i++) {
+        sum = a + b;
+        a = b;
+        b = sum;
     }
+    return b;
 }
 
 int main() {
-    // 遍历1到1000的所有数
-    for(int i = 1; i <= 1000; i++) {
-        printPerfectNumber(i);
+    double sum = 0.0;
+    for (int i = 1; i <= 20; i++) {
+        int numerator = Fibonacci(i + 1);
+        int denominator = Fibonacci(i);
+        sum += (double)numerator / denominator;
     }
-    
+    printf("%lf\n", sum);
     return 0;
 }
